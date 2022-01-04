94b871cf607759ecb5ebe62d34a90362be54c1a2
Andrei1907 <79142869+Andrei1907@users.noreply.github.com>
2022-01-05 00:42:53 +0200

    tabela adevar

--- main.py
+++ main.py
@@ -41,3 +41,37 @@ if __name__ == '__main__':
         file1.close()
         quit()
 
+    #construire tabela adevar
+    pow2 = 2
+    if(maxi >= 8):
+        ultima_col = 4
+    else: ultima_col = 3
+    lin, col = pow(2, ultima_col), ultima_col
+    tabela = [[0 for j in range(col)] for i in range(lin)]
+
+    for j in range(col-1, -1, -1):
+        for i in range(0, lin):
+            if(i % pow2 < pow2/2):
+                tabela[i][j] = 0
+            else: tabela[i][j] = 1
+        pow2 = pow2 * 2
+
+    if(ultima_col == 3):
+        afis = "A B C Output"
+    else: afis = "A B C D Output"
+
+    file1 = open("table.txt", "w")
+    file1.write(afis + "\n")
+    file1.close()
+
+    for i in range(0, lin):
+        afis = ""
+        for j in range(0, col):
+            afis = afis + str(tabela[i][j]) + " "
+        if(i in res):
+            afis = afis + "1"
+        else: afis = afis + "0"
+
+        file1 = open("table.txt", "a")
+        file1.write(afis + "\n")
+        file1.close()
